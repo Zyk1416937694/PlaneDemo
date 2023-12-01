919d404810a657985ac4d06f15005134031258e0
@zyk1002_room <zhongyukai@tmeeyou.com>
2023-12-01 14:02:18 +0800

    first commit

---
+++ app/src/main/java/com/zyk/demo/code/MainActivity.java
@@ -0,0 +1,16 @@
+package com.zyk.demo.code;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_mine_plane).setOnClickListener(v -> {
+        });
+    }
+}
